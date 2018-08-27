5e276d66929418c8586bd67fc92d1406a07984aa
al3xius <al3xius.yt@gmail.com>
2018-08-27 18:31:53 +0200

    auto push

--- speedcontrol/scripts/listener.py
+++ speedcontrol/scripts/listener.py
@@ -44,6 +44,7 @@ def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data)
     #print(data.linear.x)
     duty = data.linear.x+(50)
+    print(duty)
     my_pwm.ChangeDutyCycle(duty)
 
 
