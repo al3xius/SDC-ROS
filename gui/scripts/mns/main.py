afefa5b6a71bd4a5b7a5669f51f9abd91d2e34d1
DavidZechm <david@zechi.at>
2018-11-05 19:32:55 +0100

    v0.2 - working on cam

--- gui/scripts/mns/main.py
+++ gui/scripts/mns/main.py
@@ -4,24 +4,31 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.slider import Slider
+from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.graphics.vertex_instructions import Ellipse
 from kivy.graphics.context_instructions import Color
 from kivy.core.window import Window
+from kivy.uix.screenmanager import SlideTransition
 from kivy.garden.mapview import MapView
 import shutil
+#TODO: sortieren / kommentieren / auslagern
+
 
 #Menu Buttons
 Builder.load_file("mns.kv")
 Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/opt/opt.kv")
 Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/ccd/ccd.kv")
-Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/cav/cav.kv")
-Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/map/map.kv")
 
 # Set Window size and other Variables
-Window.size = (950, 700)  # TODO: 16:9 & touch mit display und rpi testen
+#TODO: 16:9 & touch mit display und rpi testen
+win_x = 950
+win_y = 700
+Window.size = (win_x, win_y)
 zoomLevel = 18
 cur_lat = 47.224282
 cur_lon = 15.6233008
@@ -33,39 +40,42 @@ sm = ScreenManager()
 class ScreenMAP(Screen):
     def on_enter(self):
         # Map with Zoom and Coordinates
-        mapview = MapView(zoom=zoomLevel, lat=cur_lat, lon=cur_lon)
-                          #TODO: use ros variables for coordinates && implement zoom
+        mapview = MapView(zoom=18, lat=cur_lat, lon=cur_lon)
+                          #TODO: use ros variables for coordinates
 
         # Back to Menu Button
         def changeScreen(self):
-            #TODO: change transition to "right"
+            sm.transition = SlideTransition(direction='right')
             sm.current = "MNS"
 
         backBtn = Button(text="[b]BACK[/b]", font_size="20sp", pos=(0,
                          0), size_hint=(.2, .1), background_color=(1, 1, 1, 0.45), markup=True)
         backBtn.bind(on_press=changeScreen)
-        #TODO: font-weight auf bold stellen
 
         # Add to Map layout
         self.add_widget(mapview)
         self.add_widget(backBtn)
 
+    def on_leave(self):
+        self.clear_widgets()
+
 
 class ScreenOPT(Screen):
-        """
-        # Delete Map Cache
-        def deleteCache(self):
-            files = glob.glob(
-                '/home/davidzechm/catkin_ws/src/gui/scripts/cache/*')
-            i = 0
-            for f in files:
-                try:
-                    os.remove(f)
-                    i = i+1
-                    print("Deleting File: " + str(i))
-                except IOError:
-                    print("Error")
-        """
+    """
+    # Delete Map Cache
+    def deleteCache(self):
+        files = glob.glob(
+            '/home/davidzechm/catkin_ws/src/gui/scripts/cache/*')
+        i = 0
+        for f in files:
+            try:
+                os.remove(f)
+                i = i+1
+                print("Deleting File: " + str(i))
+            except IOError:
+                print("Error")
+    """
+    pass
 
 
 class ScreenCCD(Screen):
@@ -73,7 +83,42 @@ class ScreenCCD(Screen):
 
 
 class ScreenCAV(Screen):
-    pass
+    def on_enter(self):
+        #Get Camera image
+        cam = Camera(
+            play=False, index=0, resolution=(win_x, win_y), allow_stretch=True, keep_ratio=False)
+            #TODO: Overlays
+        cam.play = True
+
+        # Back to Menu Button
+        def changeScreen(self):
+            sm.transition = SlideTransition(direction='right')
+            sm.current = "MNS"
+
+        backBtn = Button(text="[b]BACK[/b]", font_size="20sp", pos=(0,
+                         0), size_hint=(.2, .1), background_color=(1, 1, 1, 0.45), markup=True)
+        backBtn.bind(on_press=changeScreen)
+
+        # Overlay
+        overlayBtn = Button(
+            text="[b]TOGGLE OVERLAY[/b]", font_size="20sp", pos=(200,
+                                                                 0), size_hint=(.2, .1), background_color=(1, 1, 1, 0.45), markup=True)
+        overlayBtn.bind(on_press=changeScreen)
+
+        # Screenshot
+        screenBtn = Button(
+            text="[b]SCREENSHOT[/b]", font_size="20sp", pos=(400,
+                                                             0), size_hint=(.2, .1), background_color=(1, 1, 1, 0.45), markup=True)
+        screenBtn.bind(on_press=changeScreen)
+
+        # Add to Screen
+        self.add_widget(cam)
+        self.add_widget(backBtn)
+        self.add_widget(overlayBtn)
+        self.add_widget(screenBtn)
+
+    def on_leave(self):
+        self.clear_widgets()
 
 
 class ScreenMNS(Screen):
