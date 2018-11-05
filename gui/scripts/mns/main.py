35531bbe9f9806441efff7aa7ee1061175f38b74
DavidZechm <david@zechi.at>
2018-11-05 17:33:08 +0100

    control panel with working map - v0.2

--- gui/scripts/mns/main.py
+++ gui/scripts/mns/main.py
@@ -1,11 +1,17 @@
+import os
+import glob
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.uix.image import Image
+from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.graphics.vertex_instructions import Ellipse
+from kivy.graphics.context_instructions import Color
 from kivy.core.window import Window
+from kivy.garden.mapview import MapView
+import shutil
 
 #Menu Buttons
 Builder.load_file("mns.kv")
@@ -14,23 +20,52 @@ Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/ccd/ccd.kv")
 Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/cav/cav.kv")
 Builder.load_file("/home/davidzechm/catkin_ws/src/gui/scripts/map/map.kv")
 
-#Cam View
-Window.size = (950, 700)  # TODO: 16:9 testen
-
-"""
-# Car Image - Screen
-def drawCar():
-    layout = BoxLayout()
-    carImage = Image(source='scripts/assets/car.png')
-    layout.add_widget(carImage)
-    return layout
-"""
+# Set Window size and other Variables
+Window.size = (950, 700)  # TODO: 16:9 & touch mit display und rpi testen
+zoomLevel = 18
+cur_lat = 47.224282
+cur_lon = 15.6233008
 
 # Screen Manager
+sm = ScreenManager()
+
+
+class ScreenMAP(Screen):
+    def on_enter(self):
+        # Map with Zoom and Coordinates
+        mapview = MapView(zoom=zoomLevel, lat=cur_lat, lon=cur_lon)
+                          #TODO: use ros variables for coordinates && implement zoom
+
+        # Back to Menu Button
+        def changeScreen(self):
+            #TODO: change transition to "right"
+            sm.current = "MNS"
+
+        backBtn = Button(text="[b]BACK[/b]", font_size="20sp", pos=(0,
+                         0), size_hint=(.2, .1), background_color=(1, 1, 1, 0.45), markup=True)
+        backBtn.bind(on_press=changeScreen)
+        #TODO: font-weight auf bold stellen
+
+        # Add to Map layout
+        self.add_widget(mapview)
+        self.add_widget(backBtn)
 
 
 class ScreenOPT(Screen):
-    pass
+        """
+        # Delete Map Cache
+        def deleteCache(self):
+            files = glob.glob(
+                '/home/davidzechm/catkin_ws/src/gui/scripts/cache/*')
+            i = 0
+            for f in files:
+                try:
+                    os.remove(f)
+                    i = i+1
+                    print("Deleting File: " + str(i))
+                except IOError:
+                    print("Error")
+        """
 
 
 class ScreenCCD(Screen):
@@ -45,14 +80,14 @@ class ScreenMNS(Screen):
     pass
 
 
-sm = ScreenManager()
 sm.add_widget(ScreenMNS(name='MNS'))
 sm.add_widget(ScreenOPT(name='OPT'))
 sm.add_widget(ScreenCCD(name='CCD'))
 sm.add_widget(ScreenCAV(name='CAV'))
-sm.add_widget(ScreenCAV(name='MAP'))
+sm.add_widget(ScreenMAP(name='MAP'))
 
 
+# Run
 class MyApp(App):
 
     def build(self):
--- gui/scripts/opt/opt.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.button import Button
-from kivy.uix.slider import Slider
-
-root = Widget()
-root.add_widget(Button())
-slider = Slider()
-root.add_widget(slider)
