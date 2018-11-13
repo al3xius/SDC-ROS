7f6c7088818897e26a0b72f36d9c003a77088e14
al3xius.yt <al3xius.yt@gmail.com>
2018-11-13 10:02:32 +0100

    gps

--- sdc_gps/scripts/gps_node.py
+++ sdc_gps/scripts/gps_node.py
@@ -3,7 +3,9 @@
 # Simple GPS module demonstration.
 # Will wait for a fix and print a message every second with the current location
 # and other details.
-import time, serial
+import time, serial, rospy
+from sensor_msgs.msg import NavSatFix
+
 #import board
 #import busio
 
@@ -55,46 +57,61 @@ gps.send_command(b'PMTK220,1000')
 # Main loop runs forever printing the location, etc. every second.
 last_print = time.monotonic()
 
-while True:
-    # Make sure to call gps.update() every loop iteration and at least twice
-    # as fast as data comes from the GPS unit (usually every second).
-    # This returns a bool that's true if it parsed new data (you can ignore it
-    # though if you don't care and instead look at the has_fix property).
-    gps.update()
-    # Every second print out current location details if there's a fix.
-    current = time.monotonic()
+def gps_talker():
+    pub = rospy.Publisher('gps', NavSatFix, queue_size=10)
+    rospy.init_node('gps', anonymous=True)
+    msg = NavSatFix()
+    seq = 0
 
-    if current - last_print >= 1.0:
-        last_print = current
-        if not gps.has_fix:
-            # Try again if we don't have a fix yet.
-            print('Waiting for fix...')
-            continue
-        # We have a fix! (gps.has_fix is true)
-        # Print out details about the fix like location, date, etc.
-        print('=' * 40)  # Print a separator line.
-        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-            gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-            gps.timestamp_utc.tm_mday,  # struct_time object that holds
-            gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-            gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-            gps.timestamp_utc.tm_min,   # month!
-            gps.timestamp_utc.tm_sec))
-        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
-        print('Fix quality: {}'.format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
-        if gps.satellites is not None:
-            print('# satellites: {}'.format(gps.satellites))
-        if gps.altitude_m is not None:
-            print('Altitude: {} meters'.format(gps.altitude_m))
-        if gps.track_angle_deg is not None:
-            print('Speed: {} knots'.format(gps.speed_knots))
-        if gps.track_angle_deg is not None:
-            print('Track angle: {} degrees'.format(gps.track_angle_deg))
-        if gps.horizontal_dilution is not None:
-            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print('Height geo ID: {} meters'.format(gps.height_geoid))
+    
+    while not rospy.is_shutdown():
+        gps.update()
+        current = time.monotonic()
 
+        if current - last_print >= 1.0:
+            seq += 1
+            last_print = current
+
+            msg.header.seq = seq
+            msg.header.stamp = gps.timestamp_utc
+            msg.header.frame_id = 'world'
+
+            if not gps.has_fix:
+                # Try again if we don't have a fix yet.
+                msg.status.status = int(0)
+                continue
+            else:
+                msg.status.status = int(1)
+            # We have a fix! (gps.has_fix is true)
+            # Print out details about the fix like location, date, etc.
+            msg.status.service = int(0)
+            
+            # position
+            msg.latitude = float(gps.latitude)
+            msg.longitude = float(gps.longitude)
+            
+            #print('Fix quality: {}'.format(gps.fix_quality))
+            # Some attributes beyond latitude, longitude and timestamp are optional
+            # and might not be present.  Check if they're None before trying to use!
+            """if gps.satellites is not None:
+                print('# satellites: {}'.format(gps.satellites))"""
+            if gps.altitude_m is not None:
+                msg.altitude = float(gps.altitude_m)
+            """if gps.track_angle_deg is not None:
+                print('Speed: {} knots'.format(gps.speed_knots))
+            if gps.track_angle_deg is not None:
+                print('Track angle: {} degrees'.format(gps.track_angle_deg))
+            if gps.horizontal_dilution is not None:
+                print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+            if gps.height_geoid is not None:
+                print('Height geo ID: {} meters'.format(gps.height_geoid))"""
+
+            #publish
+            pub.publish(msg)
+
+
+if __name__ == '__main__':
+    try:
+        gps_talker()
+    except rospy.ROSInterruptException:
+        pass
