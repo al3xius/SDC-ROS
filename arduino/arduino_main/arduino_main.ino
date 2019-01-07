0c8f093926859dfc1a3b0ca9187c2b4306acab20
al3xius <al3xius.yt@gmail.com>
2019-01-07 12:02:38 +0100

    speedometer

--- arduino/arduino_main/arduino_main.ino
+++ arduino/arduino_main/arduino_main.ino
@@ -35,6 +35,15 @@ const int is_mega = LOW;
 
 int indicatorState = LOW;
 
+//Speedometer
+#define speedometerPin = A1;
+const int circumference = 1000; //in mm
+int maxSpeedCounter = 100;//min time (in ms) of one rotation (for debouncing)
+int speedCounter;
+int speedVal;
+long timer = 0;// time between one full rotation (in ms)
+float speedKMH = 0.00;
+
 
 //Subscriber
 void messageCb( const sdc_msgs::state& data){
@@ -97,6 +106,8 @@ void setup()
       pinMode(pullUpPins[i], INPUT_PULLUP);
     }
   }
+  //Pinput 
+  pinMode(speedometerPin, INPUT);
 
   //Output
   for (int i = 0; i < sizeof(outputPins); i++){
@@ -105,6 +116,29 @@ void setup()
     }
   }
   
+  //speedometer
+  speedCounter = maxspeedCounter;
+
+  //Source: https://www.instructables.com/id/Arduino-Bike-Speedometer/
+  // TIMER SETUP- the timer interrupt allows preceise timed measurements of the reed switch
+  //for mor info about configuration of arduino timers see http://arduino.cc/playground/Code/Timer1
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1kHz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;
+  // set timer count for 1khz increments
+  OCR1A = 1999;// = (1/1000) / ((1/(16*10^6))*8) - 1
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS11 bit for 8 prescaler
+  TCCR1B |= (1 << CS11);   
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei();//allow interrupts
+  //END TIMER SETUP
 
   nh.getHardware()->setBaud(57600);
   nh.initNode();
@@ -159,11 +193,6 @@ void loop()
     arduinoIn_msg.digital[50] = indicatorState;
   }
   
-  //timer so serial doesn't get overloaded
-  if(currentMillis - previousMillis > 20){
-    previousMillis = millis();
-    p.publish(&arduinoIn_msg);
-  }
  
   //indicator
   if(currentMillis - indicateMillis > 500){
@@ -172,6 +201,41 @@ void loop()
       indicatorState = HIGH;
     else
       indicatorState = LOW;
+  }
+
+  //speedomerter
+  ISR(TIMER1_COMPA_vect) {//Interrupt at freq of 1kHz to measure reed switch
+    speedVal = digitalRead(speedometerPin);//get val of A0
+    if (speedVal){//if reed switch is closed
+      if (speedCounter == 0){//min time between pulses has passed
+        speedKMH = (3600*float(circumference))/float(timer);//calculate km per hour
+        timer = 0;//reset timer
+        speedCounter = maxSpeedCounter;//reset speedCounter
+      }
+      else{
+        if (speedCounter > 0){//don't let speedCounter go negative
+          speedCounter -= 1;//decrement speedCounter
+        }
+      }
+    }
+    else{//if reed switch is open
+      if (speedCounter > 0){//don't let speedCounter go negative
+        speedCounter -= 1;//decrement speedCounter
+      }
+    }
+    if (timer > 2000){
+      speedKMH = 0;//if no new pulses from reed switch- tire is still, set mph to 0
+    }
+    else{
+      timer += 1;//increment timer
+    } 
+  }
+  arduinoIn_msg.analog[2] = speedKMH;
+
+  //timer so serial doesn't get overloaded
+  if(currentMillis - previousMillis > 20){
+    previousMillis = millis();
+    p.publish(&arduinoIn_msg);
   }
   
   nh.spinOnce();
