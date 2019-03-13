be60885f91d76b40b6c2daa5e54ed5ec29a926b6
DavidZechm <david@zechi.at>
2019-03-13 10:51:07 +0100

    tests

--- safety/scripts/safety.py
+++ safety/scripts/safety.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import rospy 
+import rospy
 from math import radians
 from sdc_msgs.msg import state, arduinoIn
 from sensor_msgs.msg import LaserScan
+
 
 class SafetyNode:
     def stateCallback(self, msg):
@@ -14,31 +15,32 @@ class SafetyNode:
         pass
 
     def laserCallback(self, msg):
-        angle_min = radians(self.emergencyBreakAngle + self.state.steeringAngel)
-        angle_max = radians(360) - (self.emergencyBreakAngle + self.state.steeringAngel)
+        angle_min = radians(180 - self.emergencyBreakAngle)
+        angle_max = radians(180 + self.emergencyBreakAngle)
+        self.lastScan = msg
 
-        index_0 = int(radians(0)/ float(self.lastScan.angle_increment))
-        index_360 = int(radians(360)/ float(self.lastScan.angle_increment))
+        index_0 = int(radians(0) / float(self.lastScan.angle_increment))
+        index_360 = int(radians(360) / float(self.lastScan.angle_increment))
         index_min = int(angle_min / float(self.lastScan.angle_increment))
         index_max = int(angle_max / float(self.lastScan.angle_increment))
 
-        min_n = min(self.lastScan.ranges[index_max:index_360])
-        min_p = min(self.lastScan.ranges[index_0:index_min])
+        min_n = min(self.lastScan.ranges[index_min:index_max])
+        #min_p = min(self.lastScan.ranges[index_0:index_min])
 
-        emergencyDistance = min((min_n, min_p))
+        #emergencyDistance = min((min_n, min_p))
+        emergencyDistance = min_n
 
-        if emergencyDistance < self.stopingDistance*0.9:
+        if emergencyDistance < self.stopingDistance*0.9 or emergencyDistance < 0.5:
             rospy.logerr("Emergency Break!")
-            self.state.state = "emergencyBreak"
+            self.state.mode = "emergencyBreak"
             self.state.enableSteering = True
-            self.state.steeringAngle = 0 #TODO: Steer away
+            self.state.steeringAngle = 0  # TODO: Steer away
             self.state.enableMotor = False
             self.state.throttle = 0
             self.state.direction = 0
             self.state.breaking = 100
             #self.state.light = msg.light
             self.state.indicate = "Both"
-        pass
 
         self.publish()
 
@@ -49,25 +51,22 @@ class SafetyNode:
     def __init__(self):
         rospy.init_node("safety_node")
         rospy.loginfo("Starting SafetyNode.")
-    
 
         self.stopingDistance = None
-        normBreakingDistance = rospy.get_param("/safety/normBreakingDistance", default="10")
+        normBreakingDistance = rospy.get_param(
+            "/safety/normBreakingDistance", default="10")
         normVelocity = rospy.get_param("/safety/normVelocity", default="10")
-        self.emergencyBreakAngle = rospy.get_param("/safety/emergencyBreakAngle", default="10")
-        
-        
-        self.breakingFactor = normBreakingDistance/(normVelocity*normVelocity)
+        self.emergencyBreakAngle = rospy.get_param(
+            "/safety/emergencyBreakAngle", default="10")
 
+        self.breakingFactor = normBreakingDistance/(normVelocity*normVelocity)
 
         rospy.Subscriber("/state/unchecked", state, self.stateCallback)
         rospy.Subscriber("/scan", LaserScan, self.laserCallback)
-        
-
 
         self.state = state()
         self.state_pub = rospy.Publisher("/state", state, queue_size=1)
-              
+
         pass
 
 
